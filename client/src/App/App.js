ae1b9a0623c4a25926ad734bdf904372bb5d8e5c
Isaac Thompson <shufflesninja@gmail.com>
2024-02-17 14:35:24 -0500

    Get authentication working (commit in correct directory)

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -8,7 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Sundae</h1>
-        <h2>Logged in? {isLoggedIn}</h2>
+        <img src="/icon.png" alt="Ice Cream"></img>
+        <h2>Logged in? {isLoggedIn ? "true" : "false"}</h2>
+        <h3>Profile {JSON.stringify(profile)}</h3>
         <a href="/login">Login</a>
         <a href="/logout">Logout</a>
       </header>
--- client/src/App/useServer.js
+++ client/src/App/useServer.js
@@ -1,17 +1,19 @@
-import { axios } from "axios";
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 const ENDPOINT = "http://localhost:3000";
 
 export const useServer = () => {
+  const [isLoggedIn, _setIsLoggedIn] = useState(false);
   const [profile, _setProfile] = useState(null);
-  const isLoggedIn = () => profile != null;
 
   const _getProfile = () => {
     axios
       .get(ENDPOINT + "/profile")
       .then(res => {
-        _setProfile(res);
+        console.log("Got profile");
+        _setIsLoggedIn(res.data !== "");
+        _setProfile(res.data);
       })
       .catch(e => {
         console.error("Failed to retrieve profile");
