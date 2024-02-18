5029737cfc0ae85152afbb2e494bfb6cda07d7fe
Isaac Thompson <shufflesninja@gmail.com>
2024-02-18 10:54:34 -0500

    Revert "Edit login page"
    This reverts commit 584634e7a03050f59d96415ea797d9d88f31266a.

--- client/src/App/Login/Login.js
+++ client/src/App/Login/Login.js
@@ -4,6 +4,7 @@ export const Login = () => {
   return (
     <div className="login">
       <h1>Sundae</h1>
+      <h2>Student Information Management Pseudo-System (SIMPS)</h2>
       <img src="/icon2.png" alt="Sundae Logo"></img>
       <a href="/login"><button id="loginButton" type="button">Login</button></a>
     </div>
