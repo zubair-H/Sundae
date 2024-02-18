584634e7a03050f59d96415ea797d9d88f31266a
Isaac Thompson <shufflesninja@gmail.com>
2024-02-18 09:56:19 -0500

    Edit login page

--- client/src/App/Login/Login.js
+++ client/src/App/Login/Login.js
@@ -4,7 +4,6 @@ export const Login = () => {
   return (
     <div className="login">
       <h1>Sundae</h1>
-      <h2>Student Information Management Pseudo-System (SIMPS)</h2>
       <img src="/icon2.png" alt="Sundae Logo"></img>
       <a href="/login"><button id="loginButton" type="button">Login</button></a>
     </div>
