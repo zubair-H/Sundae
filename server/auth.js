4d40653264b158dc0a4e49335312ab96f0a000f5
Isaac Thompson <shufflesninja@gmail.com>
2024-02-18 00:19:24 -0500

    Fix icon in index.html and set baseURL in auth.s

--- server/auth.js
+++ server/auth.js
@@ -5,7 +5,7 @@ export const AUTH_CONFIG = {
   authRequired: false,
   auth0Logout: true,
   secret: SECRET,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://sundaee-c5c29f94e9b3.herokuapp.com',
   clientID: 'JeSdkfkOQ5zeHA0OlUZbROb8hc4BdtJg',
   issuerBaseURL: 'https://dev-wwzvhvto6t4diopr.us.auth0.com'
 };
